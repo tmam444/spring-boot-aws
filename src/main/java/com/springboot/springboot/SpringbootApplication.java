245edd93bfe65c8c0a8aaaa32392e1b5d643082e
chung <cndfuf0159@naver.com>
2021-02-16 16:56:18 +0900

    war packaging

---
+++ src/main/java/com/springboot/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
